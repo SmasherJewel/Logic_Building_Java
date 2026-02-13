50501105a7d56bb5702339a28044387b1bcff5c1
smasher <ffadda2020@gmail.com>
2026-02-14 00:32:18 +0530

    feat: implement print 1 to n using while loop

--- Phase1_WhileLoop/src/Print_1toN.java
+++ Phase1_WhileLoop/src/Print_1toN.java
@@ -1,8 +1,24 @@
 import java.util.Scanner;
+/* Print all numbers from 1 to n using a loop. */
 
 public class Print_1toN {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        // Handling Negative Value
+        int num = Math.abs(sc.nextInt());
 
+        if (num == 0){
+            System.out.println("Enter a number greater than zero");
+            return;
+        }
+
+        int i = 1;
+        System.out.println("Printing No: ");
+        while (i <= num){
+            System.out.print(i + " ");
+            i++;
+        }
+        sc.close();
     }
 }

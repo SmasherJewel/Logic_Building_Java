7d2f2a0f5ff1d1bad9d06ba63dba6d13acfb3bcb
smasher <ffadda2020@gmail.com>
2026-02-14 20:54:37 +0530

    feat: implement fibonacci series using while loop

--- Phase1_WhileLoop/src/Q19_Fibonacci_Series.java
+++ Phase1_WhileLoop/src/Q19_Fibonacci_Series.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
+
+/* Print the Fibonacci series up to n terms */
 
 public class Q19_Fibonacci_Series {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of terms to print Fibonacci: ");
+        int num = Math.abs(sc.nextInt());
+
+        int a = 0, b = 1;
+        int i = 1;
+        System.out.println("Fibonacci Series: ");
+        while (i <= num){
+            System.out.print(a + " ");
+            int temp = a + b;
+            a = b;
+            b = temp;
+            i++;
+        }
+        sc.close();
+    }
 }

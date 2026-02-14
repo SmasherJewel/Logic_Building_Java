cf600b525c7a7b7423b2111da75ff0c513a4bfb8
smasher <ffadda2020@gmail.com>
2026-02-14 23:51:38 +0530

    feat: implement sum of factors using while loop

--- Phase1_WhileLoop/src/Q25_SumOf_Factor.java
+++ Phase1_WhileLoop/src/Q25_SumOf_Factor.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
+
+/* Find and print the sum of all factors of the given number. */
 
 public class Q25_SumOf_Factor {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number to find sum of factor of that number: ");
+        int num = Math.abs(sc.nextInt());
+
+        int i = 1;
+        int sum = 0;
+        System.out.println("Factor of " + num + " are: ");
+        while (i <= num){
+            if (num % i == 0){
+                sum += i;
+            }
+            i++;
+        }
+        sc.close();
+        System.out.println("Sum of that number factor is: " + sum);
+    }
 }

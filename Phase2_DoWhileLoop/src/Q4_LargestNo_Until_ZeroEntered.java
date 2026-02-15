047bf72a0446783a94f4253084f5e0224cde37e9
smasher <ffadda2020@gmail.com>
2026-02-15 20:36:19 +0530

    feat: add sentinel-based largest number calculation

--- Phase2_DoWhileLoop/src/Q4_LargestNo_Until_ZeroEntered.java
+++ Phase2_DoWhileLoop/src/Q4_LargestNo_Until_ZeroEntered.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
+
+/* Keep taking numbers from the user until 0 is entered,
+    then print the largest number among all inputs. */
 
 public class Q4_LargestNo_Until_ZeroEntered {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long num;
+        Long largest = null;
+
+        do {
+            System.out.println("Enter a number: ");
+            num = sc.nextLong();
+            if (num != 0){
+                if (largest == null || num > largest){
+                    largest = num;
+                }
+            }
+        }while (num != 0);
+        sc.close();
+
+        if (largest == null){
+            System.out.println("No numbers were entered.");
+        }else {
+            System.out.println("Largest number among them is: " + largest);
+        }
+    }
 }

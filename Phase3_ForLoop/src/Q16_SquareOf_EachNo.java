47f714fad614ae9b00947f612f878e430598b861
smasher <ffadda2020@gmail.com>
2026-02-16 14:39:01 +0530

    feat: implement square calculation for numbers from 1 to n

---
+++ Phase3_ForLoop/src/Q16_SquareOf_EachNo.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+/* Print the square of each number from 1 to n. */
+
+public class Q16_SquareOf_EachNo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a range you want to print square of each number: ");
+        long range = sc.nextLong();
+
+        if (range <= 0){
+            System.out.println("Please entera positive range");
+            sc.close();
+            return;
+        }
+
+        for (long i = 1; i <= range; i++) {
+            long power = i * i;
+            System.out.println(i + " * " + i + " is: " + power);
+        }
+        sc.close();
+    }
+}

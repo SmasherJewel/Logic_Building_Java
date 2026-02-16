56e7cd18ad84c67904eb958586e2d56895182292
smasher <ffadda2020@gmail.com>
2026-02-16 18:37:26 +0530

    feat: implement nested loop program to print prime numbers up to N with input validation

--- Phase4_NestedLoop/src/Q4_PrintPrimes_1toN.java
+++ Phase4_NestedLoop/src/Q4_PrintPrimes_1toN.java
@@ -8,14 +8,30 @@ public class Q4_PrintPrimes_1toN {
         System.out.println("Enter a range you want to print Prime numbers: ");
         long range = sc.nextLong();
 
-        if (range < 1){
+        if (range <= 1){
             System.out.println("There is no Prime numbers in that range");
             sc.close();
             return;
         }
 
-        if (range > 1000){
-
+        if (range > 10000){
+            System.out.println("Range too large! Please enter number below 10000.");
+            sc.close();
+            return;
         }
+
+        System.out.println("\nPrime numbers from 1 to " + range + " are:\n");
+        for (long i = 2; i <= range; i++) {
+            long count = 0;
+            for (long j = 1; j <= i; j++) {
+                if (i % j == 0){
+                    count++;
+                }
+            }
+            if (count == 2){
+                System.out.print(i + " ");
+            }
+        }
+        sc.close();
     }
 }

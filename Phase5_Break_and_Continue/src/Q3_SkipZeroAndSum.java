42d7effd585c8a18a90500a11c6aa5b46341a9e9
smasher <ffadda2020@gmail.com>
2026-02-20 00:24:33 +0530

    feat: implement sum of 5 numbers while skipping zeros using continue

---
+++ Phase5_Break_and_Continue/src/Q3_SkipZeroAndSum.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+/* Take 5 numbers as input, skip any number that is 0 using continue, and
+    calculate the sum of the remaining numbers. */
+
+public class Q3_SkipZeroAndSum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int sum = 0;
+
+        System.out.println("Enter 5 numbers:");
+
+        for (int i = 1; i <= 5; i++) {
+            int num = sc.nextInt();
+
+            if (num == 0) {
+                continue;
+            }
+            sum += num;
+        }
+        System.out.println("Sum of non-zero numbers: " + sum);
+        sc.close();
+    }
+}

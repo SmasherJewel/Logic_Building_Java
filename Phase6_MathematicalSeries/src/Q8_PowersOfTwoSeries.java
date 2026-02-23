0b9e2d2ed9eb2413d0c2aec46c110348d9e4d5a4
smasher <ffadda2020@gmail.com>
2026-02-23 15:33:59 +0530

    feat: implement powers of two series generation

---
+++ Phase6_MathematicalSeries/src/Q8_PowersOfTwoSeries.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+/* Print series of powers of two: 1 + 2 + 4 + ... + 2^n */
+
+public class Q8_PowersOfTwoSeries {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter value of n: ");
+        int n = sc.nextInt();
+
+        if (n < 0) {
+            System.out.println("Invalid input! Enter non-negative number.");
+            sc.close();
+            return;
+        }
+
+        if (n > 62) {
+            System.out.println("Value too large. It may overflow long.");
+        }
+
+        System.out.println("\nSeries:");
+
+        long value = 1;
+
+        for (int i = 0; i <= n; i++) {
+            System.out.print(value + " ");
+            value *= 2;
+        }
+
+        sc.close();
+    }
+}

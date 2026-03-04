06ba6ed359ade7929cd58608843807f722dcc760
smasher <ffadda2020@gmail.com>
2026-03-04 16:08:38 +0530

    fate: User input program to find sum of odd and even digits

---
+++ Phase-7_MixedLoop/src/Q7_SumOddEvenDigits.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Q7_SumOddEvenDigits {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        num = Math.abs(num);
+
+        int oddSum = 0;
+        int evenSum = 0;
+
+        while (num > 0) {
+
+            int digit = num % 10;
+
+            if (digit % 2 == 0) {
+                evenSum += digit;
+            } else {
+                oddSum += digit;
+            }
+
+            num /= 10;
+        }
+
+        System.out.println("Sum of odd digits: " + oddSum);
+        System.out.println("Sum of even digits: " + evenSum);
+
+        sc.close();
+    }
+}
